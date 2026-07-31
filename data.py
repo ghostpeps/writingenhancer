16f20b9111045ac73dcb71632bb83a4b5d522afa
Bhavish Chinamalli <bhavish.chinamalli@gmail.com>
2026-07-31 14:20:12 -0700

    Update data.py

--- data.py
+++ data.py
@@ -6,12 +6,14 @@ if not st.user.is_logged_in:
 
 
 with st.sidebar:
-
     if st.session_state.get("guest_mode"):
+        end_time = st.session_state.guest_login_time + timedelta(hours=24)
+        end_string = end_time.strftime("%b/%d, at %I:%M %p")
+      
         login_string = st.session_state.guest_login_time.strftime("%I:%M %p")
-        st.caption(f"⏱️ Guest session started at: {login_string}")
+        st.caption(f"Guest session started at {login_string}. You have until {end_string} before you get logged out.")
         
-    if st.button("🚪 Log Out / Exit", use_container_width=True):
+    if st.button("Log Out", icon=":material/logout:", use_container_width=True):
         st.session_state.guest_mode = False
         st.session_state.guest_login_time = None
         
