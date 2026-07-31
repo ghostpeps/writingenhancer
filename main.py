f163298c24811a8c0bd11c8422b68d81221c200e
Bhavish Chinamalli <bhavish.chinamalli@gmail.com>
2026-07-31 11:47:27 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 import streamlit as st
 
-pg = st.navigation([
-    st.Page("home.py", title="Home", icon=":material/home:"),
-    st.Page("data.py", title="Data", icon=":material/bar_chart:")
-])
+if "guest_mode" not in st.session_state:
+    st.session_state.guest_mode = False
+
+if st.user.is_logged_in or st.session_state.guest_mode:
+    pages = [
+        st.Page("home.py", title="Home", icon=":material/home:", default=True),
+        st.Page("analytics.py", title="Analytics", icon=":material/bar_chart:")
+    ]
+else:
+    pages = [
+        st.Page("login.py", title="Login", icon=":material/login:", default=True)
+    ]
+
+pg = st.navigation(pages)
 pg.run()
