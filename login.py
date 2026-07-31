e3616dfc7b814d8cf8e45bf9f7ce810758e3e56d
Bhavish Chinamalli <bhavish.chinamalli@gmail.com>
2026-07-31 13:12:28 -0700

    Update login.py

--- login.py
+++ login.py
@@ -6,12 +6,12 @@ st.write("Please log in or continue without an account to access the tutor.")
 col1, col2 = st.columns(2)
 
 with col1:
-    if st.button("Sign in with Google", use_container_width=True):
+    if st.button("Sign in with Google", icon=":material/account_circle:", use_container_width=True):
         st.login()
 
 with col2:
-    guestbtn = st.button("Practice without an account"
+    guestbtn = st.button("Practice without an account", icon=":material/account_circle_off:", use_container_width=True)
     st.write("Note that by clicking this button you will be automatically logged out within 24 hours")
-    if st.button("Practice without an account", use_container_width=True):
+    if guestbtn:
         st.session_state.guest_mode = True
         st.rerun()
