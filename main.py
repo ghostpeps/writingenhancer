cff9028173e0a62ce9fbe1d93aac4c0f3a25f74d
Bhavish Chinamalli <bhavish.chinamalli@gmail.com>
2026-07-31 14:32:53 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,15 @@
 import streamlit as st
 from datetime import datetime, timedelta
+import pytz
 
 if "guest_mode" not in st.session_state:
     st.session_state.guest_mode = False
 
 if "guest_login_time" not in st.session_state:
     st.session_state.guest_login_time = None
+
+if "user_timezone" not in st.session_state:
+    st.session_state.user_timezone = "UTC"
 
 def manual_logout():
     st.session_state.guest_mode = False
@@ -15,7 +19,9 @@ def manual_logout():
     else:
         st.rerun()
 if st.session_state.guest_mode and st.session_state.guest_login_time:
-    if datetime.now() > (st.session_state.guest_login_time + timedelta(hours=24)):
+    user_tz = pytz.timezone(st.session_state.user_timezone)
+    
+    if datetime.now(user_tz) > (st.session_state.guest_login_time + timedelta(hours=24)):
         manual_logout()
 if st.user.is_logged_in or st.session_state.guest_mode:
     pages = [
