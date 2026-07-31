2a3dea40d7cd9e26db3100f53133402bb7cf5dfa
Bhavish Chinamalli <bhavish.chinamalli@gmail.com>
2026-07-31 14:20:24 -0700

    Update home.py

--- home.py
+++ home.py
@@ -15,12 +15,14 @@ if st.button("Log out", icon=":material/logout:"):
         st.rerun()
 
 with st.sidebar:
-
     if st.session_state.get("guest_mode"):
+        end_time = st.session_state.guest_login_time + timedelta(hours=24)
+        end_string = end_time.strftime("%b/%d, at %I:%M %p")
+      
         login_string = st.session_state.guest_login_time.strftime("%I:%M %p")
-        st.caption(f"⏱️ Guest session started at: {login_string}")
+        st.caption(f"Guest session started at {login_string}. You have until {end_string} before you get logged out.")
         
-    if st.button("🚪 Log Out / Exit", use_container_width=True):
+    if st.button("Log Out", icon=":material/logout:", use_container_width=True):
         st.session_state.guest_mode = False
         st.session_state.guest_login_time = None
         
