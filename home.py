4ff6b8eac95ee408934405cfbbcf13b358d7785e
Bhavish Chinamalli <bhavish.chinamalli@gmail.com>
2026-07-31 13:28:28 -0700

    Update home.py

--- home.py
+++ home.py
@@ -13,3 +13,19 @@ if st.button("Log out", icon=":material/logout:"):
     else:
         st.session_state.guest_mode = False
         st.rerun()
+
+with st.sidebar:
+    st.write("---")
+
+    if st.session_state.get("guest_mode"):
+        login_string = st.session_state.guest_login_time.strftime("%I:%M %p")
+        st.caption(f"⏱️ Guest session started at: {login_string}")
+        
+    if st.button("🚪 Log Out / Exit", use_container_width=True):
+        st.session_state.guest_mode = False
+        st.session_state.guest_login_time = None
+        
+        if st.user.is_logged_in:
+            st.logout()
+        else:
+            st.rerun()
