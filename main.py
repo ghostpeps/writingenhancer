b1bc82ed6f04c668be98c0bce3f9b4339f62879b
Bhavish Chinamalli <bhavish.chinamalli@gmail.com>
2026-08-01 11:31:08 -0700

    Update main.py

--- main.py
+++ main.py
@@ -25,7 +25,8 @@ if st.session_state.guest_mode and st.session_state.guest_login_time:
     if datetime.now() > (st.session_state.guest_login_time + timedelta(hours=24)):
         manual_logout()
 if st.user.is_logged_in:
-    st.sidebar.image(st.user.picture)
+    if hasattr(st.user, "picture"):
+        st.image(st.user.picture, width=100)
 if st.user.is_logged_in or st.session_state.guest_mode:
     pages = [
         st.Page("home.py", title="Home", icon=":material/home:", default=True),
