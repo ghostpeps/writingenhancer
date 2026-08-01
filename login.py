451082ff12fa88329a2f334c310943398446295d
Bhavish Chinamalli <bhavish.chinamalli@gmail.com>
2026-07-31 21:25:30 -0700

    Refactor login logic for user and guest access

--- login.py
+++ login.py
@@ -1,19 +1,21 @@
 import streamlit as st
 from datetime import datetime
 
-st.title("Welcome to the English Tutor")
-st.write("Please log in or practice without an account to access the tutor.")
-
-col1, col2 = st.columns(2)
-
-with col1:
-    if st.button("Sign in with Google", icon=":material/account_circle:", use_container_width=True):
-        st.login()
-
-with col2:
-    guestbtn = st.button("Practice without an account", icon=":material/account_circle_off:", use_container_width=True)
-    st.caption("Note that by clicking this button you will be automatically logged out within 24 hours")
-    if guestbtn:
-        st.session_state.guest_mode = True
-        st.session_state.guest_login_time = datetime.now()
-        st.rerun()
+if not st.user.is_logged_in or st.session_state.guest_mode:
+    st.title("Welcome to the English Tutor")
+    st.write("Please log in or practice without an account to access the tutor.")
+    col1, col2 = st.columns(2)
+    with col1:
+        if st.button("Sign in with Google", icon=":material/account_circle:", use_container_width=True):
+            st.login()
+    
+    with col2:
+        guestbtn = st.button("Practice without an account", icon=":material/account_circle_off:", use_container_width=True)
+        st.caption("Note that by clicking this button you will be automatically logged out within 24 hours")
+        if guestbtn:
+            st.session_state.guest_mode = True
+            st.session_state.guest_login_time = datetime.now()
+            st.rerun()
+else:
+    grade = st.selectbox("Please choose your grade:", ("1", "2", "3", "4", "5", "6"), None)
+    logged_in = st.button("Submit", ":material/send:")
