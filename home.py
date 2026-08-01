f49fd32296a37929955f31eafeb3aef004492d14
Bhavish Chinamalli <bhavish.chinamalli@gmail.com>
2026-08-01 16:18:12 -0700

    Show selected grade in session state
    Display the selected grade in the session state.

--- home.py
+++ home.py
@@ -47,6 +47,7 @@ with col2:
             if grade:
                 st.session_state.selected_grade = int(grade)
                 st.rerun()
+        st.write(st.session_state.selected_grade)
 if st.user.is_logged_in:
     s1, s2 = st.sidebar.columns(2, vertical_alignment="center")
     with s1:
