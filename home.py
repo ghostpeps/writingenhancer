8612f20587e33c511f3aa8e0ab2bda1b315ea9b7
Bhavish Chinamalli <bhavish.chinamalli@gmail.com>
2026-08-01 11:18:01 -0700

    Update home.py

--- home.py
+++ home.py
@@ -24,12 +24,12 @@ with col2:
 if st.user.is_logged_in:
     s1, s2 = st.columns(2)
     with s1:
-        st.sidebar.markdown(":material/mode_heat: Some Number") #add one to streak every time submit button is clicked in test. add html to make streak logo and text. save to file
+        st.sidebar.markdown(":material/mode_heat: 0") #add one to streak every time submit button is clicked in test. add html to make streak logo and text. save to file
     with s2:
         @st.dialog("Test Score", dismissible=True)
         def show_popup_message(message_text):
             st.write(message_text)
-        if st.sidebar.button("Some Number", type="tertiary"): #save to file. make sure to add/subtract to/from score
+        if st.sidebar.button("0", type="tertiary"): #save to file. make sure to add/subtract to/from score
             show_popup_message("The test score is used to define your overall improvements. The more positive feedback you receive, the higher your score goes, and the more negative feedback you receive, the lower your score goes. Your streak also affects your test score. If your streak is higher, your score will increase faster, while reducing the amount you decrease when given negative feedback. ")
 with st.sidebar:
     if st.session_state.get("guest_mode") and st.session_state.get("guest_login_time"):
