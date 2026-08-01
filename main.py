2f26fd609cdadf27fdfa5960e9be72df3c8b3ffa
Bhavish Chinamalli <bhavish.chinamalli@gmail.com>
2026-07-31 21:51:42 -0700

    Update page selection logic for user login states

--- main.py
+++ main.py
@@ -21,11 +21,11 @@ def manual_logout():
 if st.session_state.guest_mode and st.session_state.guest_login_time:
     if datetime.now() > (st.session_state.guest_login_time + timedelta(hours=24)):
         manual_logout()
-if st.session_state.selected_grade:
-        pages = [
-            st.Page("home.py", title="Home", icon=":material/home:", default=True),
-            st.Page("data.py", title="Data", icon=":material/bar_chart:")
-        ]
+if st.user.is_logged_in or st.session_state.guest_mode:
+    pages = [
+        st.Page("home.py", title="Home", icon=":material/home:", default=True),
+        st.Page("data.py", title="Data", icon=":material/bar_chart:")
+    ]
 else:
     pages = [
         st.Page("login.py", title="Login", icon=":material/login:", default=True)
