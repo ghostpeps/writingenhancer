3a87bcecbb9d0a00d910e2ff6d8362bfdea02f48
Bhavish Chinamalli <bhavish.chinamalli@gmail.com>
2026-08-02 18:43:12 -0700

    Refactor streak calculation to use integer division
    Replaced math functions with direct integer division for streak calculations.

--- testing.py
+++ testing.py
@@ -1,5 +1,4 @@
 import streamlit as st
-import math
 
 def inc_streak(x):
   x = True
@@ -10,7 +9,7 @@ def inc_score(x, y, streak, score):
     if int(streak) == 0:
       return x - y
     else:
-      return (x - y) * math.floor(int(streak) / 2)
+      return (x - y) * int(streak) / 10
   elif x < y:
     if int(streak) == 0:
       if int(score) == 0:
@@ -18,7 +17,7 @@ def inc_score(x, y, streak, score):
       else:
         return x - y
     else:
-      return (x - y) / math.ceil(int(streak) / 2) 
+      return (x - y) / int(streak) / 10
   elif x == y:
     return 0
 
