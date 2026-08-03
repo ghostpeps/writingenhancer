43be10b526f5c037362d3781a154235e47497c91
Bhavish Chinamalli <bhavish.chinamalli@gmail.com>
2026-08-02 20:45:39 -0700

    Update home.py

--- home.py
+++ home.py
@@ -17,7 +17,7 @@ with col2:
     st.title("Tests")
     if st.user.is_logged_in:
         try:
-            with open(f"{st.user.email}.txt", "r", encoding="utf-8") as f:
+            with open(f"{st.user.email}grade.txt", "r", encoding="utf-8") as f:
                 if "G" == f.read(1):
                     y = True
         except FileNotFoundError:
@@ -25,17 +25,17 @@ with col2:
         if not y:
             grade = st.selectbox("Please choose your grade to start testing", ("1", "2", "3", "4", "5", "6"), None)
             if grade:
-                with open(f"{st.user.email}.txt", "a", encoding="utf-8") as f:
+                with open(f"{st.user.email}grade.txt", "a", encoding="utf-8") as f:
                     f.write(f"Grade {grade}")
         elif y:
             grade = st.selectbox("Change Grade", ("1", "2", "3", "4", "5", "6"), None)
             if grade:
-                with open(f"{st.user.email}.txt", "rb+") as f:
+                with open(f"{st.user.email}grade.txt", "rb+") as f:
                     f.seek(6, 0)
                     x = f"{grade}"
                     f.write(x.encode("utf-8"))
             st.write("Tests will be here")
-            with open(f"{st.user.email}.txt", "r", encoding="utf-8") as f:
+            with open(f"{st.user.email}grade.txt", "r", encoding="utf-8") as f:
                 st.write(f.read())
     else:
         if st.session_state.selected_grade not in (1, 2, 3, 4, 5, 6):
@@ -53,20 +53,22 @@ with col2:
                     st.session_state.selected_grade = int(grade)
                     st.write("Grade " + str(st.session_state.selected_grade))
                     st.rerun()
-def increaser(streak, score):
-    s1, s2 = st.sidebar.columns(2, vertical_alignment="center")
-    with s1:
-        
-        st.markdown(f":material/mode_heat: {}")
-if st.user.is_logged_in
-    s1, s2 = st.sidebar.columns(2, vertical_alignment="center")
-    with s1:
-        st.markdown(":material/mode_heat: 0") #add one to streak every time submit button is clicked in test. add html to make streak logo and text. save to file
-    with s2:
-        @st.dialog("Test Score", dismissible=True)
+
+s1, s2 = st.sidebar.columns(2, vertical_alignment="center")
+with s1:
+    if st.session_state.streak: # add reset streak and incomplete streak
+        with open(f"{st.user.email}streak.txt", "r+") as f:
+            f.seek(7)
+            z = int(f.read()) + 1
+            f.truncate()
+            f.write(f"{z}")
+        st.markdown(":color[:material/mode_heat:" + z + "]{foreground='#fa3002'}")
+        st.session_state.streak = False
+with s2:
+    @st.dialog("Test Score", dismissible=True)
         def show_popup_message(message_text):
             st.write(message_text)
-        if st.button("0", type="tertiary", use_container_width=True): #save to file. make sure to add/subtract to/from score
+        if st.button("0", type="tertiary", use_container_width=True):
             show_popup_message("The test score is used to define your overall improvements. The more positive feedback you receive, the higher your score goes, and the more negative feedback you receive, the lower your score goes. Your streak also affects your test score. If your streak is higher, your score will increase faster, while reducing the amount you decrease when given negative feedback. Complete a test every day (including diagnostics) to increase your streak. If you forget to test for just one day, your streak restarts at 0.")
 with st.sidebar:
     if st.session_state.get("guest_mode") and st.session_state.get("guest_login_time"):
