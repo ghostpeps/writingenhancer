983f1051d2d30a2a754421f5a7f19b4b97b652b7
Bhavish Chinamalli <bhavish.chinamalli@gmail.com>
2026-08-02 18:28:35 -0700

    Implement streak and score increment functions
    Added functions to increment streak and score based on comparison.

--- testing.py
+++ testing.py
@@ -1,3 +1,25 @@
 import streamlit as st
+import math
 
-st.write("testing page")
+def inc_streak(x):
+  x = True
+  return x
+
+def inc_score(x, y, streak, score):
+  if x > y:
+    if int(streak) == 0:
+      return x - y
+    else:
+      return (x - y) * math.floor(int(streak) / 2)
+  elif x < y:
+    if int(streak) == 0:
+      if int(score) == 0:
+        return 0
+      else:
+        return x - y
+    else:
+      return (x - y) / math.ceil(int(streak) / 2) 
+  elif x == y:
+    return 0
+
+st.title("testing page")
