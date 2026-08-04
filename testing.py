8600ed98d6c070ea3686140b84c0ee2325f703b3
Bhavish Chinamalli <bhavish.chinamalli@gmail.com>
2026-08-04 16:15:15 -0700

    Update testing.py

--- testing.py
+++ testing.py
@@ -2,7 +2,12 @@ import streamlit as st
 
 def inc_streak():
   st.session_state.streak = True
-
+  try:
+    with open(f"{st.user.email}strk.txt", "r+") as f:
+      f.write("y")
+  except FileNotFoundError:
+    with open(f"{st.user.email}strk.txt", "w") as f:
+      f.write("y")
 def inc_score(x, y, streak, score):
   if x > y:
     if int(streak) == 0:
