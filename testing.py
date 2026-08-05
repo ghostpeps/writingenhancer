e57ec17ade24981970bbb70d25807e789ab4e62a
Bhavish Chinamalli <bhavish.chinamalli@gmail.com>
2026-08-05 15:25:10 -0700

    Update testing.py

--- testing.py
+++ testing.py
@@ -1,28 +1,42 @@
 import streamlit as st
 
 def inc_streak():
-  st.session_state.streak = True
   try:
     with open(f"{st.user.email}strk.txt", "r+") as f:
       f.write("y")
   except FileNotFoundError:
     with open(f"{st.user.email}strk.txt", "w") as f:
       f.write("y")
-def inc_score(x, y, streak, score):
-  if x > y:
-    if int(streak) == 0:
-      st.session_state.score =  x - y
-    else:
-      st.session_state.score = (x - y) * (int(streak) / 10)
-  elif x < y:
-    if int(streak) == 0:
-      if int(score) == 0:
-        st.session_state.score = 0
+def inc_score(x, y):
+  try:
+    with open(f"{st.user.email}streak.txt", "r") as f:
+      z = f.read()
+  except FileNotFoundError:
+    with open(f"{st.user.email}streak.txt", "w") as f:
+      f.write("0")
+      z = "0"
+  try:
+    with open(f"{st.user.email}score.txt", "r") as f:
+      w = f.read()
+  except FileNotFoundError:
+    with open(f"{st.user.email}score.txt", "w") as f:
+      f.write("0")
+      w = "0"
+  with open(f"{st.user.email}score.txt", "w") as f:
+    if x > y:
+      if int(z) == 0:
+        f.write(str(x-y))
       else:
-        st.session_state.score = x - y
-    else:
-      st.session_state.score = (x - y) / (int(streak) / 10)
-  elif x == y:
-    st.session_state.score = 0
+        f.write(str((x - y) * (int(z) / 10)))
+    elif x < y:
+      if int(z) == 0:
+        if int(w) == 0:
+          f.write("0")
+        else:
+          f.write(str(x-y))
+      else:
+        f.write(str((x - y) / (int(z) / 10)))
+    elif x == y:
+      f.write("0")
 
 st.title("testing page")
