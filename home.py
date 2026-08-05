f3837374fc712fe273a82bc4fc2c0eb139c071c7
Bhavish Chinamalli <bhavish.chinamalli@gmail.com>
2026-08-05 15:15:23 -0700

    Update markdown to display streak value correctly
    Fix markdown formatting to include streak value as string.

--- home.py
+++ home.py
@@ -105,7 +105,7 @@ if st.user.is_logged_in:
                     z = 0
             with open(f"{st.user.email}streak.txt", "w") as f:
                 f.write(f"{z}")
-            st.markdown(":color[:material/mode_heat: " + z + "]{foreground='#fa3002'}")
+            st.markdown(":color[:material/mode_heat: " + str(z) + "]{foreground='#fa3002'}")
         elif v == "n" and u != "0":
             try:
                 with open(f"{st.user.email}streak.txt", "r") as f:
