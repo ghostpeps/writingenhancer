8946e23d5c3456160ae681f88778ab73ca8e39a7
Bhavish Chinamalli <bhavish.chinamalli@gmail.com>
2026-08-05 15:12:44 -0700

    Update home.py

--- home.py
+++ home.py
@@ -8,7 +8,7 @@ user_tz_str = st.context.timezone
 
 with open(f"{st.user.email}streak.txt", "w") as f:
     f.write("100")
-with opne(f"{st.user.email}strk.txt", "w") as f:
+with open(f"{st.user.email}strk.txt", "w") as f:
     f.write("y")
 if user_tz_str:
     tz_obj = pytz.timezone(user_tz_str)
