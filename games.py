32692b7c6dc07fd91a1b50f24e9b5bd4ebfd9fbc
Bhavish Chinamalli <bhavish.chinamalli@gmail.com>
2026-08-05 21:48:51 -0700

    Implement user progress check for games access
    Add functionality to check user progress and navigate to tests.

--- games.py
+++ games.py
@@ -1,3 +1,15 @@
 import streamlit as st
 
-st.title("Games")
+try:
+  with open(f"{st.user.email}strk.txt", "r") as f:
+    a = f.read()
+except FileNotFoundError:
+  with open(f"{st.user.email}strk.txt", "w") as f:
+    f.write("n")
+    a = "n"
+if a == "n":
+  st.subheader("Complete at least one test today to play games")
+  if st.button("Go to Tests", icon=":material/assignment:"):
+    st.switch_page("home.py") 
+elif a == "y":
+  st.title("Games")
