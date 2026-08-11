2440f1f98f07109a8c923c4b1d306d29d70f5804
Bhavish Chinamalli <bhavish.chinamalli@gmail.com>
2026-08-11 16:12:43 -0700

    Update data.py

--- data.py
+++ data.py
@@ -12,6 +12,114 @@ if not st.user.is_logged_in:
   st.write("View test data from today, including areas to improve on, here.")
 else:
   st.write("View test data, including tests and areas to improve on, here.")
+
+
+if st.user.is_logged_in:
+    try:
+        with open(f"{st.user.email}d.txt", "r") as f:
+            progress_value = int(f.read())
+    except FileNotFoundError:
+        with open(f"{st.user.email}d.txt", "w") as f:
+            f.write("0")
+            progress_value = 0
+    if progress_value != 100:
+        filled_part = (progress_value / 100) * 250
+        empty_part = 250 - filled_part
+        hidden_bottom = 110
+        fig = go.Figure(
+            data=[
+                go.Pie(
+                    values=[filled_part, empty_part, hidden_bottom],
+                    rotation=237, 
+                    direction="clockwise",
+                    hole=0.8,
+                    marker=dict(
+                        colors=["#FF4B4B", "#E5E5E5", "rgba(0,0,0,0)"]
+                    ),
+                    hoverinfo="skip",
+                    textinfo="none",
+                    sort=False,
+                )
+            ]
+        )
+        
+        fig.update_layout(
+            annotations=[
+                dict(
+                    text=f"{progress_value}%",
+                    x=0.5,
+                    y=0.5,
+                    font_size=42,
+                    font_weight="bold",
+                    showarrow=False,
+                ),
+                dict(
+                    text="Diagnostic Progress",
+                    x=0.5,
+                    y=0.3,
+                    font_size=14,
+                    font_color="gray",
+                    showarrow=False,
+                )
+            ],
+            height=320,
+            margin=dict(t=10, b=10, r=10, l=10),
+            showlegend=False,
+        )
+        
+        st.plotly_chart(fig, use_container_width=True)
+  s10, s20 = st.columns(2, vertical_alignment="center")
+
+  with s20:
+    if progress_value == 100:
+      filled_part = (progress_value / 100) * 250
+      empty_part = 250 - filled_part
+      hidden_bottom = 110
+      fig = go.Figure(
+          data=[
+              go.Pie(
+                  values=[filled_part, empty_part, hidden_bottom],
+                  rotation=237, 
+                  direction="clockwise",
+                  hole=0.8,
+                  marker=dict(
+                      colors=["#FF4B4B", "#E5E5E5", "rgba(0,0,0,0)"]
+                  ),
+                  hoverinfo="skip",
+                  textinfo="none",
+                  sort=False,
+              )
+          ]
+      )
+      
+      fig.update_layout(
+          annotations=[
+              dict(
+                  text=f"{progress_value}%",
+                  x=0.5,
+                  y=0.5,
+                  font_size=42,
+                  font_weight="bold",
+                  showarrow=False,
+              ),
+              dict(
+                  text="Diagnostic Progress",
+                  x=0.5,
+                  y=0.3,
+                  font_size=14,
+                  font_color="gray",
+                  showarrow=False,
+              )
+          ],
+          height=320,
+          margin=dict(t=10, b=10, r=10, l=10),
+          showlegend=False,
+      )
+      
+      st.plotly_chart(fig, use_container_width=True)
+
+
+# sidebar:
 if st.user.is_logged_in:
   s1, s2 = st.sidebar.columns(2, vertical_alignment="center")
   with s1:
