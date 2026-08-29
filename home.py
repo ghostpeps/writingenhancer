a990e62d0d9cc302a9bad5a3dd044c6126d91969
Bhavish Chinamalli <bhavish.chinamalli@gmail.com>
2026-08-29 11:53:38 -0700

    Remove guest session timeout handling
    Removed guest session timeout logic and related UI elements.

--- home.py
+++ home.py
@@ -236,20 +236,6 @@ if st.user.is_logged_in:
         if st.button(f"{w}", type="tertiary", use_container_width=True):
             show_popup_message("The test score is used to define your overall improvements. The more positive feedback you receive, the higher your score goes, and the more negative feedback you receive, the lower your score goes. Your streak also affects your test score. If your streak is higher, your score will increase faster, while reducing the amount you decrease when given negative feedback. Complete a test every day (including diagnostics) to increase your streak. If you forget to test for just one day, your streak restarts at 0.")
 with st.sidebar:
-    if st.session_state.get("guest_mode") and st.session_state.get("guest_login_time"):
-        start_time = st.session_state.guest_login_time
-        expiration_time = start_time + timedelta(hours=24)
-        current_time = datetime.now()
-        time_left = expiration_time - current_time
-        total_seconds = int(time_left.total_seconds())
-        
-        if total_seconds > 0:
-            hours = total_seconds // 3600
-            minutes = (total_seconds % 3600) // 60
-            st.metric(label="Time until automatically logged out:", value=f"{hours}:{minutes}")
-        else:
-            st.error("Session expired! Refreshing...")
-        
     if st.button("Log Out", icon=":material/logout:", use_container_width=True):
         st.session_state.guest_mode = False
         st.session_state.guest_login_time = None
